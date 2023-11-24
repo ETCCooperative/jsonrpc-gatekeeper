2ee6503ad165ea796df8c205c5d65cdaa5110467
Chris Ziogas <ziogaschr@gmail.com>
2023-11-24 17:55:05 +0200

    Better handling of IPC chunked data

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 // const cors = require('cors');
 const express = require('express');
-const ipc = require('node-ipc').default;
+const ipc = require('@node-ipc/node-ipc').default;
 
 require('dotenv').config();
 
 const GETH_IPC_NAME = 'geth';
 const GETH_IPC_PATH = process.env.GETH_IPC_PATH || '/tmp/core-geth_classic.ipc';
 
-const rpcTimeoutSeconds = 30;
 const port = process.env.PORT || 3000;
+const rpcTimeoutSeconds = 60;
 
 const app = express();
 
@@ -22,16 +22,17 @@ ipc.config.id = GETH_IPC_NAME;
 ipc.config.retry = 1500;
 ipc.config.rawBuffer = true;
 ipc.config.silent = true;
+ipc.config.sync = true; // Pass single request through IPC and wait for response
 
 ipc.connectTo(GETH_IPC_NAME, GETH_IPC_PATH, () => {
   ipc.of.geth
-    .on('connect', function () {
+    .on('connect', function() {
       console.log('Connected to Geth');
     })
-    .on('disconnect', function () {
+    .on('disconnect', function() {
       console.log('Disconnected from Geth');
     })
-    .on('error', function (error) {
+    .on('error', function(error) {
       console.error('IPC Error:', error);
     });
 });
@@ -42,9 +43,23 @@ if (process.env.WHITELISTED_METHODS) {
   whitelistedMethods = process.env.WHITELISTED_METHODS.split(',');
 }
 
-const sendRequestToGeth = (requestBody) => {
+// This will be used to generate unique ids for peers
+let jsonRpcId = 1;
+
+// Generate unique id for peer
+const generatePeerId = () => {
+  if (jsonRpcId >= Number.MAX_SAFE_INTEGER) {
+    jsonRpcId = 1;
+  }
+  return jsonRpcId++;
+};
+
+const sendRequestToGeth = requestBody => {
   // Handle byte stream till end of connection
   let buffer = Buffer.alloc(0);
+
+  let orginalRequestId = requestBody.id;
+  requestBody.id = generatePeerId();
 
   return new Promise((resolve, reject) => {
     // Timeout after some seconds
@@ -56,18 +71,27 @@ const sendRequestToGeth = (requestBody) => {
     ipc.of[GETH_IPC_NAME].emit(JSON.stringify(requestBody));
 
     // Listen for data from Geth
-    ipc.of[GETH_IPC_NAME].on('data', (data) => {
+    ipc.of[GETH_IPC_NAME].on('data', data => {
       // Concatenate new data chunk to the existing buffer
       buffer = Buffer.concat([buffer, data]);
 
-      // Check if reponse is finished
-      if (buffer.includes('\n')) {
+      // Geth doesn't have a delimiter to catch end of a JSON.
+      // For this reason we try by JSON.parse
+      // https://github.com/ethereum/go-ethereum/issues/3702
+      try {
+        let jsonString = buffer.toString();
+        const res = JSON.parse(jsonString);
+
         clearTimeout(timeout);
 
-        resolve(JSON.parse(buffer.toString()));
+        res.id = orginalRequestId;
+
+        resolve(res);
 
         // Reset the buffer for the next message
         buffer = Buffer.alloc(0);
+      } catch (err) {
+        // Wait for more data until JSON.parse performs without throwing
       }
     });
   });
@@ -86,10 +110,8 @@ app.post('/', async (req, res) => {
       res.status(403).send('Method not allowed');
     }
   } catch (err) {
-    console.error('Error processing response:', err);
-    if (!res.headersSent) {
-      res.status(500).send('Internal Server Error');
-    }
+    console.error('Error processing RPC response:', err.message);
+    res.status(500).send('Internal Server Error');
   }
 });
 
