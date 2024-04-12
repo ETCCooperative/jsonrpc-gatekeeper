ea2ed9de005ad445970ba48d0a559a54ff758baa
Chris Ziogas <ziogaschr@gmail.com>
2024-04-12 18:22:34 +0300

    Bring back health check

---
+++ health_check.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Health check is being run as a CRON
+# The reason for this is that the service is to handle the crappy IPC with core-geth
+# which is not reliable because of the streaming JSON RPC.
+# The service is being restarted every minute if the health check fails.
+#
+# */1 * * * * /usr/local/src/jsonrpc-gatekeeper/health_check.sh >> /var/log/jsonrpc_gatekeeper_health_check.log 2>&1
+
+# Load the .env file
+source "$(dirname "$0")/.env"
+
+# API URL
+API_URL="http://127.0.0.1:$PORT"
+
+NOW=$(date +"%Y-%m-%d %H:%M:%S")
+
+if ! curl --fail -s -L -H "Content-Type: application/json" --data '{"jsonrpc": "2.0", "id": 2000000, "method": "admin_peers", "params": []}' "$API_URL" > /dev/null; then
+    echo "[$NOW] Service check failed. Restarting $SYSTEMD_SERVICE..."
+    systemctl restart "$SYSTEMD_SERVICE"
+else
+    echo "[$NOW] Service check passed."
+fi
